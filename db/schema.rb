1b9c15622afe68b211b724cb932aeb5e13d56e1c
Jan <671981@gmail.com>
2015-10-10 12:23:41 +0200

    updated products page lesson 32 exit $

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010074412) do
+ActiveRecord::Schema.define(version: 20151010091500) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151010074412) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
   end
 
 end
