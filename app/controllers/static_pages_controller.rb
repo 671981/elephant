e64be9b8060225d2ecfbfe4491730f67337a4dd3
Jan <671981@gmail.com>
2015-10-19 19:32:59 +0200

    lesson 38

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,18 @@
-class StaticPagesController < ApplicationController
+	class StaticPagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
 
     @products = Product.limit(3)
   end
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+        :to => '671981@gmail.com',
+        :subject => "A new contact form message from #{@name}",
+        :body => @message).deliver
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root 'static_pages#landing_page'
 
+  post 'static_pages/thank_you'
+
   # get 'static_pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
