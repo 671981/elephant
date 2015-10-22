face64db93fda4128cec77c4524a5e452d92deb3
Jan <671981@gmail.com>
2015-10-22 23:42:39 +0200

    fix for 40

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user && current_user.admin?
+  end
+  
 end
-
-
