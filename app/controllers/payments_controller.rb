494d69c20b4f4b644b7fec92cca1ae022987278d
Jan <671981@gmail.com>
2015-10-26 22:18:40 +0100

    ex 53

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,29 @@
+class PaymentsController < ApplicationController
+def create
+  token = params[:stripeToken]
+  @product = Product.find(params[:product_id])
+  # Create the charge on Stripe's servers - this will charge the user's card
+  begin
+    charge = Stripe::Charge.create(
+      :amount => 0, # amount in cents, again
+      :currency => "usd",
+      :source => token,
+      :description => params[:stripeEmail]
+    )
+
+	    if charge.paid
+	  Order.create(
+	  	product_id: @product.id, total: @product.price, user_id: @user.id
+	  	)
+	end
+
+  rescue Stripe::CardError => e
+    # The card has been declined
+    body = e.json_body
+    err = body[:error]
+    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+  end
+  redirect_to product_path(product)
+end
+
+end
