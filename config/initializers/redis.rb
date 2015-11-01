2c6e1d08192fd54708f93d3e77819d6f51c77a06
Jan <671981@gmail.com>
2015-11-01 22:55:16 +0100

    fixing url 3

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,8 @@ $redis = nil
 if Rails.env.development?
   $redis = Redis.new(:host => 'localhost', :port => 6379)
 elsif Rails.env.production?
-  uri = URI.parse(ENV['redis://redistogo:b2b8a71d84e6cbec8088fd235c3eee6e@cobia.redistogo.com:9503/'])
-  $redis = Redis.new(:url => uri)
+  uri = URI.parse(ENV["redis://redistogo:b2b8a71d84e6cbec8088fd235c3eee6e@cobia.redistogo.com:9503/"])
+  REDIS = Redis.new(:url => uri)
 end    
+
+
